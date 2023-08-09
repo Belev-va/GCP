c2370448a9b1cafce04536fcb2236b1766693632
Belev-va <belev.job@gmail.com>
2023-08-10 02:16:59 +0400

    Add gitignore

---
+++ AMI.tf
@@ -0,0 +1,14 @@
+#create org policy
+resource "google_project_organization_policy" "howlight_policy" {
+  project = "howlight"
+  constraint = "iam.disabledAuditLoggingExemption"
+
+  boolean_policy {
+    enforced = true
+  }
+}
+#create a service account
+resource "google_service_account" "devops-sa" {
+  account_id = "devops-sa"
+  display_name = "devops service account"
+}
