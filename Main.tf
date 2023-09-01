1ba3177586487df5586194657b1b5e7101cca933
Belev-va <belev.job@gmail.com>
2023-09-02 03:19:40 +0400

    Update

--- Main.tf
+++ Main.tf
@@ -11,11 +11,39 @@ provider "google" {
   credentials = file("howlight-398ab17777d1.json")
 
   project = "howlight"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  region  = var.region
+  zone    = var.zone
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "howlight-network"
-  auto_create_subnetworks = false
+
+resource "google_compute_instance" "client" {
+  count        = var.client_count
+  name         = "${var.name}-client-${count.index}"
+  machine_type = var.client_machine_type
+  zone         = "${var.region}-${var.zone}"
+  tags         = ["client", "auto-join"]
+
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.client_disk_size_gb
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+  service_account {
+    # https://developers.google.com/identity/protocols/googlescopes
+    scopes = [
+      "https://www.googleapis.com/auth/compute.readonly",
+    ]
+  }
+  metadata = {
+    ssh-keys = "root:$(file(C:/Users/kissi/IdeaProjects/GCP/pub))"
+  }
 }
---
+++ Variable.tf
@@ -0,0 +1,60 @@
+#General variables
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "The GCP region to deploy resources in."
+}
+
+variable "zone" {
+  type        = string
+  default     = "c"
+  description = "The GCP zone to deploy resources in."
+}
+
+variable "cidr_range" {
+  type        = string
+  default     = "192.168.1.0/24"
+  description = "The IP CIDR range to use for the cluster's VPC subnetwork."
+}
+
+#Another variables
+
+variable "name" {
+  type        = string
+  default     = "howlight"
+  description = "The default name to use for resources."
+}
+variable "client_count" {
+  default = "1"
+}
+
+#Variables for servers
+
+variable "server_machine_type" {
+  type        = string
+  default     = "n1-standard-2"
+  description = "The compute engine machine type to use for server instances."
+}
+
+variable "client_machine_type" {
+  type        = string
+  default     = "n1-standard-2"
+  description = "The compute engine machine type to use for client instances."
+}
+
+variable "server_disk_size_gb" {
+  type        = string
+  default     = "50"
+  description = "The compute engine disk size in GB to use for server instances."
+}
+
+variable "client_disk_size_gb" {
+  type        = string
+  default     = "20"
+  description = "The compute engine disk size in GB to use for client instances."
+}
+variable "image" {
+  type        = string
+  default     = "centos-7-v20230809"
+  description = "The GCP image name (built with Packer)."
+}
