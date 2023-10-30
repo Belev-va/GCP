0948bdb00ce49fa0d76584b04885e02e9f1a0a57
Belev-va <belev.job@gmail.com>
2023-10-31 02:06:49 +0400

    Update

--- Main.tf
+++ Main.tf
@@ -20,12 +20,24 @@ provider "google" {
   }
 }*/
 
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080", "8000", "8888", "9990", "5432"]
+  }
+
+  target_tags = ["app"]
+}
+
 resource "google_compute_instance" "client" {
   count        = var.client_count
   name         = "${var.name}-client-${count.index}"
   machine_type = var.client_machine_type
   zone         = "${var.region}-${var.zone}"
-  tags         = ["client", "auto-join"]
+  tags         = ["client", "auto-join", "app", "http-server", "https-server"]
 
   allow_stopping_for_update = true
 
@@ -55,7 +67,7 @@ resource "google_compute_instance" "server" {
   name         = "${var.name}-server-${count.index}"
   machine_type = var.server_machine_type
   zone         = "${var.region}-${var.zone}"
-  tags         = ["server", "auto-join"]
+  tags         = ["client", "auto-join", "app", "http-server", "https-server"]
 
   allow_stopping_for_update = true
 
@@ -80,7 +92,7 @@ resource "google_compute_instance" "jenkins" {
   name         = "${var.name}-jenkins-${count.index}"
   machine_type = var.client_machine_type
   zone         = "${var.region}-${var.zone}"
-  tags         = ["jenkis", "auto-join"]
+  tags         = ["client", "auto-join", "app", "http-server", "https-server"]
 
   allow_stopping_for_update = true
 
@@ -95,7 +107,17 @@ resource "google_compute_instance" "jenkins" {
     access_config {}
   }
   metadata = {
-    ssh-keys = "howlight:${file("root.pub")}"
+    "ssh-keys" = <<EOT
+        "howlight:${file("root.pub")}"
+        "howlight:${file("root2.pub")}"
+    EOT
+    #ssh-keys = "howlight:${file("root.pub")}"
+
     enable-oslogin : "FALSE"
   }
+ # tags = [
+  #  "${lower(var.name)}",
+  #  "${lower(var.environment)}",
+   # "${lower(var.orchestration)}"
+  #]
 }
